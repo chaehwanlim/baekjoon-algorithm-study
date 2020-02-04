ad089192e1c65035a94b0eb09d7bbf1582598c8c
Chaehwan Lim <chlim428@gmail.com>
2020-02-04 19:22:47 +0900

    Create 2231 브루트포스-분해합.cpp

---
+++ 2231 브루트포스-분해합.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int i = 1;
+    int sum, temp;
+
+    while (1) {
+        sum = i;
+        temp = i;
+
+        while (temp) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+
+        if (sum == n || i == n) {
+            break;
+        }
+
+        i++;
+    }
+
+    if (i == n)
+        cout << 0;
+    else
+    {
+        cout << i;
+    }
+
+    return 0;
+}
